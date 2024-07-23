f898523fead70a02b657c4705d4517eb68269bd8
ghannoum74 <abdelrahmangh74@gmail.com>
2024-07-23 22:43:40 +0300

    remove get route

--- src/App.jsx
+++ src/App.jsx
@@ -6,30 +6,8 @@ import Campus from "./components/Campus";
 import Testimonials from "./components/Testimonials";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import { useSelector } from "react-redux";
-import axios from "axios";
-import { useEffect } from "react";
 
 const App = () => {
-  const data = {
-    Fullname: useSelector((state) => state.fullData.Fullname),
-    PhoneNumber: useSelector((state) => state.fullData.PhoneNumber),
-    Email: useSelector((state) => state.fullData.Email),
-    Details: useSelector((state) => state.fullData.Details),
-  };
-  console.log(data);
-
-  const fetchData = async () => {
-    await axios
-      .get("http://localhost:5000/all-users")
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err));
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
   return (
     <>
       <Navbar />
